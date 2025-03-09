7e489f46f4660106cfd2d429e63bd0c5510757eb
Sadam <msadamramli@gmail.com>
2025-03-09 10:59:23 +0700

    init project

---
+++ src/controllers/dummy.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+export default {
+  dummy(req: Request, res: Response) {
+    res.status(200).json({
+      message: "Success hit endpoint /dummy",
+      data: "OK",
+    });
+  },
+};
