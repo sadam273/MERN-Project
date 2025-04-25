c7f82efc6bcb6bd761d10588000a4bec3099355d
Sadam.ramli <muhammad.sr@ad-ins.com>
2025-04-25 10:20:14 +0700

    part 11

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import * as Yup from "yup";
 import UserModel from "../models/user.model";
 import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middlewares/auth.middleware";
 
 type TRegister = {
   fullname: string;
@@ -99,10 +101,35 @@ export default {
         });
       }
 
+      const token = generateToken({
+        id: userByIdentifier._id,
+        role: userByIdentifier.role,
+      });
+
       res.status(200).json({
         message: "login success",
-        data: userByIdentifier,
+        data: token,
       })
+
+    } catch (error) {
+      const err = error as unknown as Error;
+
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+  async me(req: IReqUser, res: Response){
+    try {
+      
+      const user = req.user;
+      const result = await UserModel.findById(user?.id);
+
+      res.status(200).json({
+        message: "Login sukses",
+        data: result,
+      });
 
     } catch (error) {
       const err = error as unknown as Error;
